a55a6955325faf91035d7f5cbc78b038da0cbf8c
mahesh <maheshh.mahii.98@gmail.com>
2024-06-08 13:44:26 +0530

    ADDED: variables with it's type

--- main.go
+++ main.go
@@ -10,4 +10,14 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	// fmt.Printf("Type of conferenceName %T\n", conferenceName)
+
+	var userName string
+	var userTickets int
+
+	userName = "Mahesh"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
