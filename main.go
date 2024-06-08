f709563369de713be3d2fcd8dd41aad6d215a22f
mahesh <maheshh.mahii.98@gmail.com>
2024-06-08 11:47:14 +0530

    Added: varibales and welcome message to our homepage

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var confernceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", confernceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets here to attend")
 }
